755eb644e8e0bfbc3feb313a811c6c3de9b891ec
john-rigas <jmr294@georgetown.edu>
2019-10-20 17:36:30 +0100

    fixed bid box

--- lib/backalley/bridge.ex
+++ lib/backalley/bridge.ex
@@ -10,6 +10,21 @@ defmodule Backalley.Bridge do
 
   @rankings ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
   @suits ["H", "C", "D", "S"]
+  @rankmap %{
+    "2" => 0,
+    "3" => 1,
+    "4" => 2,
+    "5" => 3,
+    "6" => 4,
+    "7" => 5,
+    "8" => 6,
+    "9" => 7,
+    "T" => 8,
+    "J" => 9,
+    "Q" => 10,
+    "K" => 11,
+    "A" => 12
+  }
 
   defstruct trump: nil, cardmap: %{}
 
@@ -197,7 +212,7 @@ defmodule Backalley.Bridge do
       new_suit != inc_suit ->
         inc
 
-      new_rank < inc_rank ->
+      Map.get(@rankmap, new_rank) < Map.get(@rankmap, inc_rank) ->
         inc
 
       true ->
